834a0fa82cff7430e04a2f6bbd52f1318fe525c4
SAIPRANAY-GANGULA <gangulasaipranay@gmail.com>
2023-01-07 11:38:52 +0530

    function component

--- app.js
+++ app.js
@@ -1,76 +1,27 @@
-/*
- *
- * Parcel( Bundlers ) Functionalities
- *
- * HMR - Hot Module Replacement
- * File Watcher algorithm: C++
- * Bundling
- * Minify
- * Cleaning you code
- * Dev abd Production Build
- * Super Fast build algorithms
- * Image Optimization
- * Caching while development
- * Compression
- * Compatibility with older versions: polyfills
- * Https on dev
- * Consistent Hashing Algorithms
- * Zero Config
- *
- * Browser list config: https://browserslist.dev/
- */
-
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { createRoot } from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "heading1",
-  },
-  "Heading 1 !"
-);
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "heading2",
-  },
-  "Heading 2 !"
-);
-const div = React.createElement(
-  "div",
-  {
-    id: "heading2",
-  },
-  [heading1, heading2]
+// Function Component
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
 );
 
+// Composing Components
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      {
+        Title()
+        // We can write any Javascript code here
+      }
+      <h2>Namaste React functional component</h2>
+      <h2>This is a h2 tag</h2>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-//passing react element inside the root
-root.render(div);
 
-//////////////////////////////////////////////////////////////////////////////
-//
-// const person1 = {
-//   name: "person1",
-//   print: function () {
-//     console.log(this);
-//   },
-// };
-//
-// const person2 = {
-//   name: "person2",
-//   print: () => {
-//     console.log(this);
-//   },
-// };
-//
-// person1.print(); //person1
-// person1.print.call(); //window
-// person1.print.call(this); //window
-// person1.print.call(person2); //person2
-//
-// person2.print(); // window
-// person2.print.call(); //window
-// person2.print.call(this); //window
-// person2.print.call(person1); // window
+root.render(<HeaderComponent />);
